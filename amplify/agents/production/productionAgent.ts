55b6d7541214b933e3f8e067d555ddfc46f2c120
tohoko.tj <msynyo@gmail.com>
2025-06-16 10:23:19 +0900

    adding another maint data to prod kb

--- amplify/agents/production/productionAgent.ts
+++ amplify/agents/production/productionAgent.ts
@@ -337,15 +337,8 @@ export function productionAgentBuilder(scope: Construct, props: ProductionAgentP
    // S3データソースを追加
     const s3docsDataSourcePigging = petroleumEngineeringKnowledgeBase.addS3DataSource({
         bucket: props.s3Bucket,
-        dataSourceName: "a4e-kb-ds-s3-maint-pigging",
-        inclusionPrefixes: ['maintenance-agent/pigging/'],
-        dataDeletionPolicy: cdkLabsBedrock.DataDeletionPolicy.RETAIN,
-        chunkingStrategy: cdkLabsBedrock.ChunkingStrategy.HIERARCHICAL_TITAN
-    });
-    const s3docsDataSourceRepair = petroleumEngineeringKnowledgeBase.addS3DataSource({
-        bucket: props.s3Bucket,
-        dataSourceName: "a4e-kb-ds-s3-maint-repair",
-        inclusionPrefixes: ['maintenance-agent/repair/'],
+        dataSourceName: "a4e-kb-ds-s3-maint-maintainance",
+        inclusionPrefixes: ['maintenance-agent/'],
         dataDeletionPolicy: cdkLabsBedrock.DataDeletionPolicy.RETAIN,
         chunkingStrategy: cdkLabsBedrock.ChunkingStrategy.HIERARCHICAL_TITAN
     });
