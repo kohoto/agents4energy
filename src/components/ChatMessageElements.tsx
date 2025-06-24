f7509f0c2c8acf9ce4cc668d108a110feb182cf4
tohoko.tj <msynyo@gmail.com>
2025-06-25 01:14:43 +0900

    changing the date format on charts

--- src/components/ChatMessageElements.tsx
+++ src/components/ChatMessageElements.tsx
@@ -329,7 +329,7 @@ export default function ChatUIMessage(props: ChatUIMessageProps) {
                 tooltipFormat: 'PP',
                 displayFormats: {
                   // day のフォーマット: m/d hh:mm
-                  day: 'M/d HH:mm',                
+                  day: 'YYYY/M',                
                 },
               },
               title: {
