894ead39a18588ff0c8cfb25d86747347db72394
tohoko.tj <msynyo@gmail.com>
2025-06-25 05:37:47 +0900

    changing the date format for figures

--- src/components/ChatMessageElements.tsx
+++ src/components/ChatMessageElements.tsx
@@ -329,7 +329,7 @@ export default function ChatUIMessage(props: ChatUIMessageProps) {
                 tooltipFormat: 'PP',
                 displayFormats: {
                   // day のフォーマット: m/d hh:mm
-                  day: 'YYYY/M',                
+                  day: 'yyyy/M',                
                 },
               },
               title: {
